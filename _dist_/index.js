2e67831cfb44fc2dde06cdc41475ec5f6889cf83
juanarangot <juanarangot@users.noreply.github.com>
2022-04-23 00:39:31 +0000

    deploy: 106eb8f2eda9c186d0040ee4e6fa5a30d5770e58

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,33 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-// Codigo del archivo principal de JavaScript
+// TODO: agregar formulario que permita buscar por nombre o tematica de imgenes, y el usuario pueda ir agregando imagenes
+
+const createImageNode = () => {
+  // Se crea el <div> contenedor de la imagen
+  const imageContainer = document.createElement('div');
+  imageContainer.className = "p-4";
+
+  // Se crea el nuevo elemento de <img>
+  const image = document.createElement('img');
+  // clase de la imagen
+  image.className = "mx-auto rounded-md bg-gray-300 card";
+  // dimensiones de la imagen
+  image.height = 350;
+  image.width = 350;
+  // Direccion de la imagen
+  image.src = "https://rickandmortyapi.com/api/character/avatar/322.jpeg";
+
+  // Se agrega el elemento <img> al <div> contenedor
+  imageContainer.appendChild(image);
+  return imageContainer;
+}
+
+
+// Se crea una nueva imagen
+const newImage = createImageNode();
+
+// Nodo principal donde se insertan las imagenes
+const mainNode = document.querySelector("#images");
+
+mainNode.appendChild(newImage);
