b8eaa5bcf80f9f0b57e0ecfc1a5c16216c5d6d28
juanarangot <juanarangot@users.noreply.github.com>
2022-04-26 07:14:05 +0000

    deploy: 5478b7fe43174f3141f01412080d6701205976ce

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,20 +1,66 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
 // API endpoint
-const URL = "https://api.unsplash.com"
+const URL = "https://api.unsplash.com";
+// const xoauthURL = "https://unsplash.com/oauth/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=code";
+const access_token = "DN1szLs1ahAVP09UbQhum4O0hWumWUsTo_E8awrmlJQ";
+// const oauthURL = "https://unsplash.com/oauth/authorize?client_id=WLd665Y8DzWCBC55jZ8p94RPliXgzslaYYPYTXuEODM&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code";
 const appNode = document.querySelector("#app");
 
 // API credentials
 const USER = "juanmaracho10@gmail.com";
 const API_KEY = "WLd665Y8DzWCBC55jZ8p94RPliXgzslaYYPYTXuEODM";
 
+
+// Trabajando con el formulario de búsqueda
+let formulario = document.querySelector("#queryForm");
+formulario.className = "bg-white shadow-md rounded px-8 pt-6 pb-4 mb-4";
+formulario.placeholder = "Busca una imagen aqui";
+
+
+
+// Obtener y retorna el valor del formulario enviado
+const boton = document.querySelector("#submitButton");
+
+// formulario.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   alert();
+//   let valor = document.querySelector("#queryForm").value;
+//   console.log(`Ha ingresado: ${valor}`);
+//   return valor
+// });
+
+function getQuery() {
+boton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let valor = document.querySelector("#queryForm").value;
+  console.log(`Ha ingresado: ${valor}`);
+  return valor
+});
+}
+
+// boton.onclick.preventDefault();
+const prueba = document.querySelector("#prueba");
+
+
+// function getQuery() {
+//   boton.addEventListener("click", (evento) => {
+//     // Previene el comportamiento por defecto del formulario para que no se envíe 2 veces
+//     evento.preventDefault();
+//     alert()
+
+//     let valor = document.querySelector("#queryForm");
+//     console.log(`Ha ingresado: ${valor}`);
+//     return valor
+//   });
+// }
+
+
+// parametro de busqueda temporal para la prueba
+const queryWord = getQuery();
 // Web API fetch
 // Conectarnos al servidor
 window
-  .fetch(`${URL}/photos/?client_id=${API_KEY}`, {
+  // .fetch(`${URL}/photos/?client_id=${API_KEY}`, {
+  .fetch(`${URL}/search/photos/?query=${queryWord}&per_page=2&access_token=${access_token}`, {
   // .fetch(`${URL}`, {
     method: "GET",
     withCredentials: true,
@@ -31,7 +77,6 @@ window
       console.log(data);
       let images = [];
       // data.forEach(element => {
-        // console.log(element.links.download);
           // Se crea el <div> contenedor de la imagen
           const imageContainer = document.createElement('div');
           imageContainer.className = "p-4";
@@ -41,13 +86,16 @@ window
           // clase de la imagen
           image.className = "mx-auto rounded-md bg-gray-300 card";
           // dimensiones de la imagen
-          // image.height = 350;
-          // image.width = 350;
+          image.height = 350;
+          image.width = 350;
           // Direccion de la imagen
           // image.src = element.links.download;
 
-          // image.src = data[0].links.download;
-          image.src = data[0].urls.raw + "&h=350&w=350";
+          // image.src = data[0].urls.raw + "&h=350&w=350";
+
+          // Devuelve la primera imagen que coincida con la busqueda
+          image.src = data.results[1].urls.raw;
+
           // image.src = data[0].urls.raw;
 
           // Se agrega el elemento <img> al <div> contenedor
