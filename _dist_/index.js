eae194511d5a9ee1bf8a1b80796ea2c5ba7db507
juanarangot <juanarangot@users.noreply.github.com>
2022-04-28 03:25:59 +0000

    deploy: eac0b60fdcbe408478eaa80ca9c8d68a0ecffd23

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,82 +1,56 @@
 // API endpoint
 const URL = "https://api.unsplash.com";
-// const xoauthURL = "https://unsplash.com/oauth/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=code";
-const access_token = "DN1szLs1ahAVP09UbQhum4O0hWumWUsTo_E8awrmlJQ";
-// const oauthURL = "https://unsplash.com/oauth/authorize?client_id=WLd665Y8DzWCBC55jZ8p94RPliXgzslaYYPYTXuEODM&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code";
-const appNode = document.querySelector("#app");
 
 // API credentials
-const USER = "juanmaracho10@gmail.com";
 const API_KEY = "WLd665Y8DzWCBC55jZ8p94RPliXgzslaYYPYTXuEODM";
+const access_token = "DN1szLs1ahAVP09UbQhum4O0hWumWUsTo_E8awrmlJQ";
 
+// Nodo principal donde se agregaran las imagenes
+const appNode = document.querySelector("#app");
+
+// Obtener y retorna el valor del formulario enviado
+const boton = document.querySelector("#submitButton");
 
 // Trabajando con el formulario de búsqueda
 let formulario = document.querySelector("#queryForm");
 formulario.className = "bg-white shadow-md rounded px-8 pt-6 pb-4 mb-4";
 formulario.placeholder = "Busca una imagen aqui";
 
-
-
-// Obtener y retorna el valor del formulario enviado
-const boton = document.querySelector("#submitButton");
-
-// formulario.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   alert();
-//   let valor = document.querySelector("#queryForm").value;
-//   console.log(`Ha ingresado: ${valor}`);
-//   return valor
+// Presiona el boton en caso de que el usuario presione enter (keycode 13)
+// formulario.addEventListener("keyup", (e) => {
+//   // Asegura que el usuario presione enter (keycode 13)
+//   if (e.keyCode === 13) {
+//     // le da click al boton ejejecutando la funcion correspondiente
+//     boton.click();
+//   }
 // });
 
-function getQuery() {
-boton.addEventListener("click", (e) => {
-  e.preventDefault();
-  let valor = document.querySelector("#queryForm").value;
-  console.log(`Ha ingresado: ${valor}`);
-  return valor
-});
-}
-
-// boton.onclick.preventDefault();
-const prueba = document.querySelector("#prueba");
 
 
-// function getQuery() {
-//   boton.addEventListener("click", (evento) => {
-//     // Previene el comportamiento por defecto del formulario para que no se envíe 2 veces
-//     evento.preventDefault();
-//     alert()
-
-//     let valor = document.querySelector("#queryForm");
-//     console.log(`Ha ingresado: ${valor}`);
-//     return valor
-//   });
-// }
 
 
-// parametro de busqueda temporal para la prueba
-const queryWord = getQuery();
 // Web API fetch
-// Conectarnos al servidor
-window
+const fetchImages = (query) => {
+  window
+  // Conectarnos al servidor
   // .fetch(`${URL}/photos/?client_id=${API_KEY}`, {
-  .fetch(`${URL}/search/photos/?query=${queryWord}&per_page=2&access_token=${access_token}`, {
-  // .fetch(`${URL}`, {
-    method: "GET",
-    withCredentials: true,
-    // headers: {
-    //   "Authorization": `Client-ID ${API_KEY}`,
-    //   "Content-Type": "application/json"
-    // }
-  })
-    .then(resp => resp.json())
+    .fetch(`${URL}/search/photos/?query=${query}&per_page=1&access_token=${access_token}`, {
+    // .fetch(`${URL}`, {
+      method: "GET",
+      withCredentials: true,
+      // headers: {
+        //   "Authorization": `Client-ID ${API_KEY}`,
+      //   "Content-Type": "application/json"
+      // }
+    })
+      .then(resp => resp.json())
 
-    .then((data) => {
+      .then((data) => {
 
-      console.log("ha funcionado")
-      console.log(data);
-      let images = [];
-      // data.forEach(element => {
+        console.log("ha funcionado")
+        console.log(data);
+        let images = [];
+        // data.forEach(element => {
           // Se crea el <div> contenedor de la imagen
           const imageContainer = document.createElement('div');
           imageContainer.className = "p-4";
@@ -94,27 +68,77 @@ window
           // image.src = data[0].urls.raw + "&h=350&w=350";
 
           // Devuelve la primera imagen que coincida con la busqueda
-          image.src = data.results[1].urls.raw;
+          image.src = data.results[0].urls.raw;
 
           // image.src = data[0].urls.raw;
 
           // Se agrega el elemento <img> al <div> contenedor
           imageContainer.append(image);
           images.push(imageContainer);
-      // })
+          // })
 
-      // Se agrega el array de imagenes al contenedor principal
-      appNode.append(...images);
-    })
+          // Se agrega el array de imagenes al contenedor principal
+          appNode.append(...images);
+        })
 
-      .catch(function(error) {
-      console.log("NO se pudo conectar")
-      console.log(error);
-  });
+        .catch(function(error) {
+          console.log("NO se pudo conectar")
+          console.log(error);
+        });
+};
 
 
-// TODO: agregar formulario que permita buscar por nombre o tematica de imgenes, y el usuario pueda ir agregando imagenes
+// function getQuery() {
+boton.addEventListener("click", (e) => {
+  // Evita que se envie el formulario y se recargue la pagina
+  // alert("dentro del boton")
+  e.preventDefault();
+  // Obtiene el valor del input
+  // let valor = document.querySelector("#queryForm").value.trim();
+  let valor = formulario.value;
+  console.log(`Ha ingresado: ${valor}`);
 
+  // Selecciono el elemento para poder agregar imagenes
+  // Funciona si seleeciono por id o tag, pero no por clase
+  // let agregaImagenes = document.getElementsByTagName("fieldset")[0];
+  let agregaImagenes = document.getElementById("lastBlock")
+
+  // Comprueba si el valor del input es vacio
+  if (valor === "") {
+    alert("ingresa una busqueda valida");
+    agregaImagenes.style = "display: none";
+  }
+  else {
+    // defino el tipo de atributo 'display' para que se muestre el bloque que estaba oculto
+    agregaImagenes.style = "display: block";
+    // se le pasa el valor del input al metodo 'fetchImages' como argumento
+    fetchImages(valor);
+    // console.log(`Buscando imagenes con la busqueda: ${valor}`);
+
+    // return valor
+  }
+});
+// }
+
+
+
+// Se agrega la funcion al evento 'load'
+// window.addEventListener("load", getQuery);
+// getQuery();
+
+// const prueba = document.querySelector("#prueba");
+// prueba.addEventListener("submit", (e) => {
+//   e.preventDefault();
+// });
+
+
+
+
+
+
+
+      // TODO: agregar formulario que permita buscar por nombre o tematica de imgenes, y el usuario pueda ir agregando imagenes
+      
 // const createImageNode = () => {
 //   // Se crea el <div> contenedor de la imagen
 //   const imageContainer = document.createElement('div');
