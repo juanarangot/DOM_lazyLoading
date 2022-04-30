e78895d3d1095907d0aa304ab5471b22fb51d878
juanarangot <juanarangot@users.noreply.github.com>
2022-04-30 06:09:49 +0000

    deploy: 0400f06e28751d58847b1ebd94803429110d700d

--- _dist_/index.js
+++ _dist_/index.js
@@ -13,7 +13,6 @@ const boton = document.querySelector("#submitButton");
 // Boton para agregar imagenes
 const botonInterno = document.querySelector("#addImage");
 botonInterno.addEventListener("click", (e) => {
-  // e.preventDefault();
   agregarImagenes(formulario.value.trim());
 });
 
@@ -22,15 +21,6 @@ botonInterno.addEventListener("click", (e) => {
 let formulario = document.querySelector("#queryForm");
 formulario.className = "bg-white shadow-md rounded px-8 pt-6 pb-4 mb-4";
 formulario.placeholder = "Busca una imagen aqui";
-
-// Presiona el boton en caso de que el usuario presione enter (keycode 13)
-// formulario.addEventListener("keyup", (e) => {
-//   // Asegura que el usuario presione enter (keycode 13)
-//   if (e.keyCode === 13) {
-//     // le da click al boton ejejecutando la funcion correspondiente
-//     boton.click();
-//   }
-// });
 
 
 // Datos de la solicitud de la API
@@ -92,8 +82,9 @@ let dataRequestObject = {
 const agregarImagenes = (query) => {
 
   const imageNode = () => {
-    // TODO: se puede usar shift, para eliminar el primer elemento de la lista y retornarlo. y asi agregarlo al dom
+    // se puede usar shift, para eliminar el primer elemento de la lista y retornarlo. y asi agregarlo al dom
     let elemento = dataRequestObject.resultados.shift();
+    // TODO: revisar el error que se genera aqui con el primer resultado
     console.log("ANTES");
     console.log(dataRequestObject.resultados);
     console.log(elemento);
@@ -117,7 +108,6 @@ const agregarImagenes = (query) => {
     // Se agrega el elemento <img> al <div> contenedor
     imageContainer.append(image);
 
-
     appNode.appendChild(imageContainer);
     console.log("Se ha agregado la imagen al DOM")
   }
@@ -129,7 +119,7 @@ const agregarImagenes = (query) => {
     console.log("TRATANDO CON EL OBJETO")
 
     // TODO: aqui se genera la imagen para agregarla al DOM
-    // imageNode();
+    imageNode();
   }
   // Se comprueba si ya se hizo la solicitud de la API anteriormente
   else if (dataRequestObject.estadoInicial) {
@@ -139,7 +129,7 @@ const agregarImagenes = (query) => {
     dataRequestObject.hacerSolicitud(); // Se hace la solicitud de la API
     // Se cambia el estado inicial a false para que no se vuelva a hacer la solicitud
     dataRequestObject.estadoInicial = false;
-    // imageNode();
+    imageNode();
 
     // TODO: hacer la funcion que compruebe si se han terminado las imagenes
   }
@@ -147,88 +137,19 @@ const agregarImagenes = (query) => {
   else {
     alert("No se encontraron resultados para tu busqueda, intenta con otra palabra");
   }
-  imageNode();
-
 }
-
-
-// Obtiene y retorna una lista con 20 resultados de la busqueda
-const fetchImages = (query, cantidad = 10) => {
-  window
-  // Conectarnos al servidor
-  // Web API fetch
-  // .fetch(`${URL}/photos/?client_id=${API_KEY}`, {
-    .fetch(`${URL}/search/photos/?query=${query}&per_page=${cantidad}&access_token=${access_token}`, {
-    // .fetch(`${URL}`, {
-      method: "GET",
-      withCredentials: true,
-      // headers: {
-        //   "Authorization": `Client-ID ${API_KEY}`,
-      //   "Content-Type": "application/json"
-      // }
-    })
-      .then(resp => resp.json())
-
-      .then((data) => {
-
-        console.log("ha funcionado")
-        console.log(data);
-        let imagenes = [];
-        // Se itera sobre la lista de imagenes recibidas
-        // data.forEach(element => {
-          // Se crea el <div> contenedor de la imagen
-          const imageContainer = document.createElement('div');
-          imageContainer.className = "p-4";
-
-          // Se crea el nuevo elemento de <img>
-          const image = document.createElement('img');
-          // clase de la imagen
-          image.className = "mx-auto rounded-md bg-gray-300 card";
-          // dimensiones de la imagen
-          image.height = 350;
-          image.width = 350;
-          // Direccion de la imagen
-          // image.src = element.links.download;
-
-          // image.src = data[0].urls.raw + "&h=350&w=350";
-
-          // Devuelve la primera imagen que coincida con la busqueda
-          image.src = data.results[0].urls.raw;
-
-          // image.src = data[0].urls.raw;
-
-          // Se agrega el elemento <img> al <div> contenedor
-          imageContainer.append(image);
-          imagenes.push(imageContainer);
-          // })
-
-        // });
-          // TODO: cambiar esta parte para que se agreguen las imagenes individualmente cuando se pulse el boton de agregar imagen
-          // Se retorna el array con los resultados de la busqueda
-          return imagenes;
-          // appNode.append(...imagenes);
-        })
-
-        .catch(function(error) {
-          console.log("NO se pudo conectar")
-          console.log(error);
-        });
-};
 
 
 // FUNCION PRINCIPAL
 // function getQuery() {
 boton.addEventListener("click", (e) => {
   // Evita que se envie el formulario y se recargue la pagina
-  // alert("dentro del boton")
   e.preventDefault();
   // Obtiene el valor del input
-  // let valor = document.querySelector("#queryForm").value.trim();
   let valor = formulario.value.trim();
 
   // Selecciono el elemento para poder agregar imagenes
   // Funciona si seleeciono por id o tag, pero no por clase
-  // let agregaImagenes = document.getElementsByTagName("fieldset")[0];
   let agregaImagenes = document.getElementById("lastBlock")
 
   // Comprueba si el valor del input es vacio
@@ -241,64 +162,8 @@ boton.addEventListener("click", (e) => {
     console.log(`Ha ingresado: ${valor}`);
     // defino el tipo de atributo 'display' para que se muestre el bloque que estaba oculto
     agregaImagenes.style = "display: block";
-    // se le pasa el valor del input al metodo 'fetchImages' como argumento
-    // fetchImages(valor);
-    // agregarImagenes(valor);
+
     // Se restablecen los valores del objeto para una nueva solicitud
     dataRequestObject.resetData();
-    // console.log(`Buscando imagenes con la busqueda: ${valor}`);
-
-    // return valor
   }
 });
-// }
-
-
-
-// Se agrega la funcion al evento 'load'
-// window.addEventListener("load", getQuery);
-// getQuery();
-
-// const prueba = document.querySelector("#prueba");
-// prueba.addEventListener("submit", (e) => {
-//   e.preventDefault();
-// });
-
-
-
-
-
-
-
-      // TODO: agregar formulario que permita buscar por nombre o tematica de imgenes, y el usuario pueda ir agregando imagenes
-      
-// const createImageNode = () => {
-//   // Se crea el <div> contenedor de la imagen
-//   const imageContainer = document.createElement('div');
-//   imageContainer.className = "p-4";
-
-//   // Se crea el nuevo elemento de <img>
-//   const image = document.createElement('img');
-//   // clase de la imagen
-//   image.className = "mx-auto rounded-md bg-gray-300 card";
-//   // dimensiones de la imagen
-//   image.height = 350;
-//   image.width = 350;
-//   // Direccion de la imagen
-//   image.src = "https://rickandmortyapi.com/api/character/avatar/322.jpeg";
-
-//   // Se agrega el elemento <img> al <div> contenedor
-//   imageContainer.appendChild(image);
-//   return imageContainer;
-// }
-
-
-// Se crea una nueva imagen
-// const newImage = createImageNode();
-
-// Nodo principal donde se insertan las imagenes
-// const mainNode = document.querySelector("#images");
-// TODO: arreglar esto luego con el bucle para que no haya doble agregacion de elementos
-// mainNode.appendChild(newImage);
-
-// appNode.appendChild(newImage);
